c48ba13a9b442684def797c8b2fe7fc361bf03d4
Brad <bfern94@yahoo.com>
2020-12-22 11:32:43 +0000

    feature engineering

--- scripts/03_manipulate.R
+++ scripts/03_manipulate.R
@@ -1,17 +1,21 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(mgcv)
 
 train <- read_csv(file.path(here::here(), "data/external/train.csv"))
 
 train <- train %>%
   select(MSZoning, LotArea, Neighborhood, BldgType, OverallQual, OverallCond, YearBuilt,
-         YearRemodAdd, BsmtFullBath, BsmtHalfBath, FullBath, HalfBath, GarageCars,
-         TotalBsmtSF, "1stFlrSF", "2ndFlrSF", MoSold, YrSold, SalePrice) %>%
+         YearRemodAdd, GarageCars, TotalBsmtSF, "1stFlrSF", "2ndFlrSF",
+         MoSold, YrSold, SalePrice) %>%
   rename(firstFlrSF = "1stFlrSF", secondFlrSF = "2ndFlrSF")
 
 train %>%
-    mutate(interior_area =  TotalBsmtSF + firstFlrSF + secondFlrSF) %>%
-    ggplot(aes(x = interior_area, y = SalePrice)) +
-    geom_point() +
-    geom_smooth()
+    mutate(
+      interiorArea =  TotalBsmtSF + firstFlrSF + secondFlrSF,
+      logSalePrice = log(SalePrice)
+    ) %>%
+    select(-c(TotalBsmtSF, firstFlrSF, secondFlrSF, SalePrice))
+
+write_csv(train, file.path(here::here(), "data/interim/train.csv"))
