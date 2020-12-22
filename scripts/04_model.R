e8f358234215e1a54d6b7254fe921ed137b24d6c
Brad <bfern94@yahoo.com>
2020-12-22 12:12:23 +0000

    update file name

--- scripts/04_model.R
+++ scripts/04_model.R
@@ -7,4 +7,4 @@ model <- lm(logSalePrice ~ 1, train)
 train_with_preds <- train %>%
     mutate(pred = as.numeric(predict(model, train)))
 
-saveRDS(train_with_preds, file.path(here::here(), "data/interim/train_preds.rds"))
+saveRDS(train_with_preds, file.path(here::here(), "data/interim/train_with_preds.rds"))
